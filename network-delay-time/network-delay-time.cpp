2ba9a88d3b6a52fad38eed80e5fd612a6fffddf8
Sri Hari Chidella <42217251+sri-hari-chidella@users.noreply.github.com>
2022-02-07 00:55:37 +0530

    Time: 120 ms, Memory: 41 MB - LeetHub

---
+++ network-delay-time/network-delay-time.cpp
@@ -0,0 +1,34 @@
+#define pii pair<int,int>
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int K) {
+        vector<vector<pair<int,int>>> graph(n);
+        for (int i=0;i<times.size();i++){
+            graph[times[i][0]-1].push_back({times[i][1]-1,times[i][2]});
+        }
+        vector<int> visited(n,0);
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        pq.push({0,K-1});
+        int ans=0;
+        while (!pq.empty()){
+            int dist=pq.top().first;int node=pq.top().second;
+            // cout<<dist<<endl;
+            pq.pop();
+            if (visited[node]==0){
+                ans=max(dist,ans);
+                visited[node]=1;
+                for (int i=0;i<graph[node].size();i++){
+                    if (!visited[graph[node][i].first]){
+                        // cout<<dist+graph[node][i].second<<" "<<graph[node][i].first<<endl;
+                        pq.push({dist+graph[node][i].second,graph[node][i].first});
+                    }
+                }
+            }
+        }
+        for (int i=0;i<n;i++){
+            if (visited[i]==0)return -1;
+        }
+        return ans;
+    }
+};
