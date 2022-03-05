c18915fe4fa884a18d0704ccb598cfe304117c50
Sri Hari Chidella <42217251+sri-hari-chidella@users.noreply.github.com>
2022-03-05 16:52:34 +0530

    Time: 35 ms (80.56%), Space: 70.7 MB (98.00%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -13,7 +13,6 @@ public:
     void addNums(ListNode* l1, ListNode* l2, int &carry){
         if (l1==NULL)return;
         addNums(l1->next,l2->next,carry);
-        // cout<<l1->val<<" "<<l2->val<<" "<<carry<<endl;
         int temp=(l1->val+l2->val+carry)%10;
         carry=(l1->val+l2->val+carry)/10;
         l1->val=temp;
@@ -46,7 +45,6 @@ public:
         ListNode* ans = new ListNode();
         int carry=0;
         addNums(temp,l2,carry);
-        // return l1;
         addCarry(l1,temp,carry);
         if (carry!=0){
             ListNode* ans=new ListNode(carry);ans->next=l1;
