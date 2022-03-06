b493f3691a3254accfaa6e5a5942434bcd236324
Sri Hari Chidella <42217251+sri-hari-chidella@users.noreply.github.com>
2022-03-06 15:17:56 +0530

    Time: 96 ms (68.36%), Space: 18.2 MB (23.28%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -4,9 +4,12 @@ public:
         vector<long long int> dp(amount+1,INT_MAX);
         sort(A.begin(),A.end());
         dp[0]=0;
-        for (int i=0;i<A.size();i++){
-            for (int j=A[i];j<=amount;j++){
-                dp[j]=min(dp[j-A[i]]+1,dp[j]);
+        for (int i=1;i<=amount;i++){
+            // cout<<i<<endl;
+            for (int j=0;j<A.size();j++){
+                if (A[j]>i)break;
+                // cout<<i<<" "<<j<<endl;
+                dp[i]=min(dp[i],dp[i-A[j]]+1);
             }
         }
         return dp[amount]==INT_MAX?-1:dp[amount];
